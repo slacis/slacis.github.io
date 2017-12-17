e78149c7fb879f8aea5c4457ab7239f7cbf0e632
simon <slacis@student.unimelb.edu.au>
2017-12-17 18:33:13 +1100

    Added portfolio page

---
+++ js/main.js
@@ -0,0 +1,23 @@
+
+// Toggle project information
+$(document).ready(function(){
+    $(".project-click").each(function(){
+        $( this ).click(function(){
+            console.log($(this).children()[1])
+            $(this).children(".recent-projects-content").slideToggle("slow");
+        })
+    });
+});
+
+// Buttons
+$("#projects").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".recent-projects-area").offset().top
+    }, 1000);
+});
+
+$("#contacts").click(function() {
+    $('html, body').animate({
+        scrollTop: $(".contacts").offset().top
+    }, 1000);
+});
